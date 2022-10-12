fd64aa3922221ddb590b20343d880dc0ee756ee6
曾文馨 <cengwenxin@dyn-160-39-255-137.dyn.columbia.edu>
2022-10-11 21:36:00 -0400

    submit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Assignment 1
+
+## Team Members
+1. name: Xiaoyu Tian (xt2261)
+2. name: Wenxin Zeng (wz2581)
+
+## Zip File Structure
+1. Frontend code: frontend.zip
+2. All Lambda Functions: lambda_functions
+	* LF0: LF0.zip
+	* LF1: LF1.zip
+	* LF2: LF2.zip
+	* importDynamoDB.zip: lambda function to import yelp scraped restaurants data into DynamoDB table and OpenSearch.
+3. YAML file generated through API Gateway: api_yaml.yml
+4. readme
+5. Yelp scraper script: yelp.py
---
+++ yelp.py
@@ -0,0 +1,120 @@
+API_KEY = "*******"
+
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+
+
+# Defaults for our simple example.
+DEFAULT_TERM = 'Chinese'
+DEFAULT_LOCATION = 'Manhattan'
+SEARCH_LIMIT = 50
+
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        api_key (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        dict: The JSON response from the request.
+
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search(api_key, term, location, offset):
+    """Query the Search API by a search term and location.
+
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT,
+        'offset': offset
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+
+    Args:
+        business_id (str): The ID of the business to query.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key)
+
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    data_list = {"total": 0, "businesses": []}
+    for offset in range(0, 1000, 50):
+        response = search(API_KEY, term, location, offset)
+        businesses = response.get('businesses')
+        data_list["businesses"].extend(businesses)
+        data_list["total"] += len(businesses)
+
+    file_name = term+".json"
+    with open(file_name, 'w') as json_file:
+        json.dump(data_list, json_file)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM,
+                        type=str, help='Search term (default: %(default)s)')
+    parser.add_argument('-l', '--location', dest='location',
+                        default=DEFAULT_LOCATION, type=str,
+                        help='Search location (default: %(default)s)')
+
+    input_values = parser.parse_args()
+
+    try:
+        query_api(input_values.term, input_values.location)
+    except HTTPError as error:
+        sys.exit(
+            'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
+                error.code,
+                error.url,
+                error.read(),
+            )
+        )
+
+
+if __name__ == '__main__':
+    main()
